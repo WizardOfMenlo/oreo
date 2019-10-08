50b3596bd2714a1723b98b3e0f3b983fe8b22635
Giacomo Fenzi <giacomofenzi@outlook.com>
2019-10-08 17:49:19 +0200

    Fixed scanner to have multiple line comments

--- src/scanner.rs
+++ src/scanner.rs
@@ -14,79 +14,104 @@ pub enum ScannedItem<'a> {
 }
 
 pub fn scan(input: &str) -> impl Iterator<Item = Scanned> {
-    input
-        .lines()
-        .enumerate()
-        .map(|(i, line)| {
-            scan_line(line).map(move |scan| Scanned {
-                inner: scan,
-                line: i,
-            })
-        })
-        .flatten()
-}
-
-fn scan_line(line: &str) -> impl Iterator<Item = ScannedItem> {
-    LineScannerIt { line, pos: 0 }
+    LineScannerIt {
+        input,
+        pos: 0,
+        line_no: 0,
+    }
 }
 
 pub struct LineScannerIt<'a> {
-    line: &'a str,
+    input: &'a str,
     pos: usize,
+    line_no: usize,
+}
+
+fn count_new_lines(s: &str) -> usize {
+    s.as_bytes().iter().filter(|&&s| s == b'\n').count()
 }
 
 impl<'a> Iterator for LineScannerIt<'a> {
-    type Item = ScannedItem<'a>;
+    type Item = Scanned<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let remaining = &self.line[self.pos..];
+        let remaining = &self.input[self.pos..];
+
+        // Skip the first whitespace chars
         let first_non_whitespace_index = remaining.find(|c: char| !c.is_whitespace());
         if remaining.is_empty() || first_non_whitespace_index.is_none() {
             return None;
         }
 
-        let remaining = &remaining[first_non_whitespace_index.unwrap()..];
-        self.pos += first_non_whitespace_index.unwrap();
+        let first_non_whitespace_index = first_non_whitespace_index.unwrap();
+        self.line_no += count_new_lines(&remaining[..first_non_whitespace_index]);
 
+        let remaining = &remaining[first_non_whitespace_index..];
+        self.pos += first_non_whitespace_index;
+
+        // Get the next chars
         let mut chars = remaining.chars().peekable();
         let first_char = chars.next().unwrap();
 
+        let start_line_no = self.line_no;
+
         if first_char == '"' {
-            let (result, read) = parse_string(remaining);
+            let (result, read, lines) = parse_string(remaining);
             self.pos += read;
-            return Some(result);
+            self.line_no += lines;
+            return Some(Scanned {
+                inner: result,
+                line: start_line_no,
+            });
         } else if first_char == '{' {
             let second = chars.peek();
             if let Some('-') = second {
-                let (result, read) = parse_comment(remaining);
+                let (result, read, lines) = parse_comment(remaining);
                 self.pos += read;
-                return Some(result);
+                self.line_no += lines;
+                return Some(Scanned {
+                    inner: result,
+                    line: start_line_no,
+                });
             }
         }
 
-        let (result, read) = parse_block(remaining);
+        let (result, read, lines) = parse_block(remaining);
         self.pos += read;
-        Some(result)
+        self.line_no += lines;
+        Some(Scanned {
+            inner: result,
+            line: start_line_no,
+        })
     }
 }
 
-fn parse_comment(input: &str) -> (ScannedItem, usize) {
+fn parse_comment(input: &str) -> (ScannedItem, usize, usize) {
     let chars_pairs = input.chars().zip(input.chars().skip(1));
     let mut index = None;
     for (i, (first, second)) in chars_pairs.enumerate() {
         match (first, second) {
-            ('-', '}') => index = Some(i),
+            ('-', '}') => {
+                index = Some(i);
+                break;
+            }
             _ => continue,
         }
     }
 
+    let lines = count_new_lines(index.map(|i| &input[2..i]).unwrap_or_else(|| &input[2..]));
+
     match index {
-        Some(i) => (ScannedItem::Comment(&input[2..i]), i + 2),
-        None => (ScannedItem::UnclosedComment(&input[2..]), input.len()),
+        Some(i) => (ScannedItem::Comment(&input[2..i]), i + 2, lines),
+        None => (
+            ScannedItem::UnclosedComment(&input[2..]),
+            input.len(),
+            lines,
+        ),
     }
 }
 
-fn parse_string(input: &str) -> (ScannedItem, usize) {
+fn parse_string(input: &str) -> (ScannedItem, usize, usize) {
     let mut next_is_escape = false;
     let mut index = None;
     for (i, ch) in input.chars().enumerate().skip(1) {
@@ -101,13 +126,15 @@ fn parse_string(input: &str) -> (ScannedItem, usize) {
         next_is_escape = false;
     }
 
+    let lines = count_new_lines(index.map(|i| &input[1..i]).unwrap_or_else(|| &input[1..]));
+
     match index {
-        Some(i) => (ScannedItem::Str(&input[1..i]), i + 1),
-        None => (ScannedItem::UnclosedStr(&input[1..]), input.len()),
+        Some(i) => (ScannedItem::Str(&input[1..i]), i + 1, lines),
+        None => (ScannedItem::UnclosedStr(&input[1..]), input.len(), lines),
     }
 }
 
-fn parse_block(input: &str) -> (ScannedItem, usize) {
+fn parse_block(input: &str) -> (ScannedItem, usize, usize) {
     let first_whitespace_index = input
         .find(|c: char| c.is_whitespace())
         .unwrap_or_else(|| input.len());
@@ -116,7 +143,7 @@ fn parse_block(input: &str) -> (ScannedItem, usize) {
         .find(|c: char| c == '"' || c == '{')
         .unwrap_or_else(|| input.len());
     let to_read = std::cmp::min(first_whitespace_index, first_problematic);
-    (ScannedItem::Rest(&input[0..to_read]), to_read)
+    (ScannedItem::Rest(&input[0..to_read]), to_read, 0)
 }
 
 #[cfg(test)]
@@ -182,6 +209,42 @@ mod tests {
     #[test]
     fn scan_real_life_with_comments() {
         let input = "program fib;\r\nbegin\r\nvar n;{-a comment here-}\r\nvar first := 0;\r\nvar second :=1;\r\nvar next;\r\nvar c :=0 ;\r\nprint \"enter the number of terms\";\r\nget n;\r\nwhile ( c < n)\r\nbegin\r\nif ( c <= 1)\r\nthen begin next := c; end\r\nelse begin\r\n next := first + second;\r\n second := next;\r\nend\r\nprint next;\r\nc := c + 1;\r\nend\r\nend\r\n";
+        let res: Vec<_> = scan(input).collect();
+        assert_debug_snapshot!(res);
+    }
+
+    #[test]
+    fn scan_oreo_8() {
+        let input = r#"
+        {- will parse OK -}
+program test
+                                   
+begin                              
+                                   
+	var n;
+	var first := 0;  
+	var second :=1; 
+	var next;                           
+	var c :=0;
+	
+    print "enter the number of terms";
+                 {- comment in body -}
+    get n;  
+    
+    while ( first < (second > 32)  )
+    begin
+
+       if ( 42 < 100 ) then begin n := 5;  end
+       			   else begin n := 46; end;
+                        {- comment
+                        over
+                        multiple 
+                        lines -}
+                                  
+    c := c + 1;	    
+	end;
+end    
+        "#;
         let res: Vec<_> = scan(input).collect();
         assert_debug_snapshot!(res);
     }
