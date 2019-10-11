07d6660d4fe46df2d046cccde66aa8162d2a0133
Giacomo Fenzi <giacomofenzi@outlook.com>
2019-10-11 20:43:44 +0200

    Added preliminary stress

---
+++ src/bin/stress.rs
@@ -0,0 +1,18 @@
+use criterion::black_box;
+use oreo::parse;
+
+fn generate_program_from_size(s: usize) -> String {
+    format!(
+        "program fib begin {} end",
+        std::iter::repeat("var x := x;")
+            .take(s)
+            .collect::<Vec<_>>()
+            .join("")
+    )
+}
+
+fn main() {
+    let input = generate_program_from_size(16384);
+    let result = parse(&input);
+    black_box(result.unwrap());
+}
