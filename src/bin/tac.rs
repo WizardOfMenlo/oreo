37bc84c7a081772d50ad515e22755544066d69d7
Giacomo Fenzi <giacomofenzi@outlook.com>
2019-11-13 23:31:11 +0100

    Added cmdline TAC gen

---
+++ src/bin/tac.rs
@@ -0,0 +1,28 @@
+use oreo::ast::AST;
+use oreo::lexer::lexicalize;
+use oreo::lexer::scanner::scan;
+use oreo::parser::parse;
+use oreo::ast::syntax::Program;
+use oreo::codegen::tac::TACBuilder;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Args {
+    /// The input to parse
+    #[structopt(short, long)]
+    input: String,
+}
+
+fn main() {
+    std::env::set_var("RUST_BACKTRACE", "1");
+    let opt = Args::from_args();
+
+    let input = &opt.input;
+    // Note, no error handling here
+    let node = parse(lexicalize(scan(input)));
+    let ast = AST::new(node, input);
+    let tac = TACBuilder::new(&ast).build(Program::new(ast.db().start_id()));
+
+    println!("{}", tac);
+}
--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,3 +1,3 @@
 //! Codegen generation
 
-mod tac;
+pub mod tac;
