b88955057eb199b1ee9e838fa2f4e3569ffec9e8
Giacomo Fenzi <giacomofenzi@outlook.com>
2019-11-20 01:47:41 +0100

    Added error handling for example

--- src/bin/hla.rs
+++ src/bin/hla.rs
@@ -7,6 +7,11 @@ use oreo::lexer::scanner::scan;
 use oreo::parser::parse;
 use structopt::StructOpt;
 
+use oreo::range::{Ranged, RangedObject};
+use oreo::lexer::tokens::Token;
+use oreo::lexer::error::format_lexical_error;
+use oreo::parser::error::format_syntax_error;
+
 #[derive(Debug, StructOpt)]
 struct Args {
     /// The input to parse
@@ -16,11 +21,41 @@ struct Args {
 
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
-    let opt = Args::from_args()
+    let opt = Args::from_args();
     let input = &opt.input;
 
-    // Note, no error handling here
-    let node = parse(lexicalize(scan(input)));
+    let tokens : Vec<_> = lexicalize(scan(input)).collect();
+
+    tokens
+        .iter()
+        .filter(|t| t.inner().is_error())
+        .for_each(|error| {
+            if let Token::Error(e) = error.inner() {
+                println!(
+                    "{}",
+                    format_lexical_error(error.clone().map(|_| e), &opt.input)
+                );
+            }
+        });
+
+    let node = parse(tokens.into_iter());
+
+    node.iter_breadth_first().for_each(|n| {
+        if n.ty().is_error() {
+            println!(
+                "{}",
+                format_syntax_error(
+                    RangedObject::new(n.ty().unwrap_err(), n.range().clone()),
+                    &opt.input
+                )
+            )
+        }
+    });
+
+    if node.iter_breadth_first().any(|n| n.ty().is_error()) {
+        println!("Fix the above errors");
+        return;
+    }
 
     let ast = AST::new(node, input);
 
